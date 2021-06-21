f79aeefa8c0e9f01cba7039db22f24b847f362e7
Matheus <34069604+Matheuscruztj@users.noreply.github.com>
2021-06-20 22:16:52 -0300

    Initial commit

---
+++ 2_volumes/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Mensagens</title>
+    </head>
+    <body>
+        <h1>Escreva sua mensagem</h1>
+        <form action="process.php" method="POST">
+            <input type="text" name="message" id="message">
+            <input type="submit" value="Enviar Mensagem">
+        </form>
+    </body>
+</html>
